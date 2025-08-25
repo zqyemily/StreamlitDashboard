659cac0b6d3db04899e7686dfbfde5cdabe6bad9
zqyemily <qianyun.zhou@outlook.com>
2025-08-25 14:54:44 +0800

    day16

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,17 @@
 import streamlit as st
 
+st.title('Customizing the theme of Streamlit apps')
 
-st.header('st.latex')
+st.write('Contents of the `.streamlit/config.toml` file of this app')
 
-st.latex(r'''
-     a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
-     \sum_{k=0}^{n-1} ar^k =
-     a \left(\frac{1-r^{n}}{1-r}\right)
-     ''')
+st.code("""
+[theme]
+primaryColor="#F39C12"
+backgroundColor="#2E86C1"
+secondaryBackgroundColor="#AED6F1"
+textColor="#FFFFFF"
+font="monospace"
+""")
+
+number = st.sidebar.slider('Select a number:', 0, 10, 5)
+st.write('Selected number from slider widget is:', number)
