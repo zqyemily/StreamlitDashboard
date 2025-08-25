d176351b1b82ae6a4132ffc916dc602fe1c1c0e2
zqyemily <qianyun.zhou@outlook.com>
2025-08-25 14:29:12 +0800

    day12

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,18 @@
 import streamlit as st
 
-st.header('st.multiselect')
+st.header('st.checkbox')
 
-options = st.multiselect(
-     'What are your favorite colors',
-     ['Green', 'Yellow', 'Red', 'Blue'],
-     ['Yellow', 'Red'])
+st.write ('What would you like to order?')
 
-st.write('You selected:', options)
+icecream = st.checkbox('Ice cream')
+coffee = st.checkbox('Coffee')
+cola = st.checkbox('Cola')
+
+if icecream:
+     st.write("Great! Here's some more 🍦")
+
+if coffee: 
+     st.write("Okay, here's some coffee ☕")
+
+if cola:
+     st.write("Here you go 🥤")
