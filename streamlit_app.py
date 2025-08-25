ebb412ca8c86cb5b3aca244f966eea58c671b891
zqyemily <qianyun.zhou@outlook.com>
2025-08-25 13:41:51 +0800

    day8

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,35 +1,39 @@
-import numpy as np
-import altair as alt
-import pandas as pd
 import streamlit as st
+from datetime import time, datetime
 
-st.header('pico x dashboard')
+st.header('st.slider')
 
 # Example 1
 
-st.write('pico x *dashboard* :sunglasses:')
+st.subheader('Slider')
+
+age = st.slider('How old are you?', 0, 130, 25)
+st.write("I'm ", age, 'years old')
 
 # Example 2
 
-st.write(1234)
+st.subheader('Range slider')
+
+values = st.slider(
+     'Select a range of values',
+     0.0, 100.0, (25.0, 75.0))
+st.write('Values:', values)
 
 # Example 3
 
-df = pd.DataFrame({
-     'first column': [1, 2, 3, 4],
-     'second column': [10, 20, 30, 40]
-     })
-st.write(df)
+st.subheader('Range time slider')
+
+appointment = st.slider(
+     "Schedule your appointment:",
+     value=(time(11, 30), time(12, 45)))
+st.write("You're scheduled for:", appointment)
 
 # Example 4
 
-st.write('Below is a DataFrame:', df, 'Above is a dataframe.')
+st.subheader('Datetime slider')
 
-# Example 5
-
-df2 = pd.DataFrame(
-     np.random.randn(200, 3),
-     columns=['a', 'b', 'c'])
-c = alt.Chart(df2).mark_circle().encode(
-     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
-st.write(c)
+start_time = st.slider(
+     "When do you start?",
+     value=datetime(2020, 1, 1, 9, 30),
+     format="MM/DD/YY - hh:mm")
+st.write("Start time:", start_time)
