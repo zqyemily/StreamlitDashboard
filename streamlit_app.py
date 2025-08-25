4bafd350f3d825b5868b100cad477180b4175bf8
zqyemily <qianyun.zhou@outlook.com>
2025-08-25 15:34:29 +0800

    day23

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,44 +1,30 @@
 import streamlit as st
 
-st.title('st.form')
+st.title('st.experimental_get_query_params')
 
-# Full example of using the with notation
-st.header('1. Example of using `with` notation')
-st.subheader('Coffee machine')
+with st.expander('About this app'):
+  st.write("`st.experimental_get_query_params` allows the retrieval of query parameters directly from the URL of the user's browser.")
 
-with st.form('my_form'):
-    st.subheader('**Order your coffee**')
-
-    # Input widgets
-    coffee_bean_val = st.selectbox('Coffee bean', ['Arabica', 'Robusta'])
-    coffee_roast_val = st.selectbox('Coffee roast', ['Light', 'Medium', 'Dark'])
-    brewing_val = st.selectbox('Brewing method', ['Aeropress', 'Drip', 'French press', 'Moka pot', 'Siphon'])
-    serving_type_val = st.selectbox('Serving format', ['Hot', 'Iced', 'Frappe'])
-    milk_val = st.select_slider('Milk intensity', ['None', 'Low', 'Medium', 'High'])
-    owncup_val = st.checkbox('Bring own cup')
-
-    # Every form must have a submit button
-    submitted = st.form_submit_button('Submit')
-
-if submitted:
-    st.markdown(f'''
-        ☕ You have ordered:
-        - Coffee bean: `{coffee_bean_val}`
-        - Coffee roast: `{coffee_roast_val}`
-        - Brewing: `{brewing_val}`
-        - Serving type: `{serving_type_val}`
-        - Milk: `{milk_val}`
-        - Bring own cup: `{owncup_val}`
-        ''')
-else:
-    st.write('☝️ Place your order!')
+# 1. Instructions
+st.header('1. Instructions')
+st.markdown('''
+In the above URL bar of your internet browser, append the following:
+`?firstname=Jack&surname=Beanstalk`
+after the base URL `http://share.streamlit.io/dataprofessor/st.experimental_get_query_params/`
+such that it becomes 
+`http://share.streamlit.io/dataprofessor/st.experimental_get_query_params/?firstname=Jack&surname=Beanstalk`
+''')
 
 
-# Short example of using an object notation
-st.header('2. Example of object notation')
+# 2. Contents of st.experimental_get_query_params
+st.header('2. Contents of st.experimental_get_query_params')
+st.write(st.experimental_get_query_params())
 
-form = st.form('my_form_2')
-selected_val = form.slider('Select a value')
-form.form_submit_button('Submit')
 
-st.write('Selected value: ', selected_val)
+# 3. Retrieving and displaying information from the URL
+st.header('3. Retrieving and displaying information from the URL')
+
+firstname = st.experimental_get_query_params()['firstname'][0]
+surname = st.experimental_get_query_params()['surname'][0]
+
+st.write(f'Hello **{firstname} {surname}**, how are you?')
