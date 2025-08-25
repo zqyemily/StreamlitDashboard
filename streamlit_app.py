0c21091d052e207140263c7dc56f556c039873b9
zqyemily <qianyun.zhou@outlook.com>
2025-08-25 13:48:20 +0800

    day11

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 
-st.header('st.selectbox')
+st.header('st.multiselect')
 
-option = st.selectbox(
-     'What is your favorite color?',
-     ('Blue', 'Red', 'Green'))
+options = st.multiselect(
+     'What are your favorite colors',
+     ['Green', 'Yellow', 'Red', 'Blue'],
+     ['Yellow', 'Red'])
 
-st.write('Your favorite color is ', option)
+st.write('You selected:', options)
