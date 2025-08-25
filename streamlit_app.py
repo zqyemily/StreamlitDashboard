aa3afa89a82ff3e5077945fdd7e005e2538749f2
zqyemily <qianyun.zhou@outlook.com>
2025-08-25 14:49:03 +0800

    day15

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,10 @@
 import streamlit as st
-import pandas as pd
-import ydata_profiling
-from streamlit_pandas_profiling import st_profile_report
 
-st.header('`streamlit_pandas_profiling`')
 
-df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+st.header('st.latex')
 
-pr = df.profile_report()
-st_profile_report(pr)
+st.latex(r'''
+     a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+     \sum_{k=0}^{n-1} ar^k =
+     a \left(\frac{1-r^{n}}{1-r}\right)
+     ''')
